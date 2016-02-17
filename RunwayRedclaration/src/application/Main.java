5521ada431b3cacbf218ab705037ee7678d47fce
jal3g14 <jal3g14@N08.ecs.soton.ac.uk>
2016-02-17 20:29:09 +0000

    Test

---
+++ RunwayRedclaration/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		 try {
+	            AnchorPane page = (AnchorPane) FXMLLoader.load(getClass().getClassLoader().getResource("RunwayFXML.fxml"));
+	            Scene scene = new Scene(page);
+	            primaryStage.setScene(scene);
+	            primaryStage.setTitle("FXML is Simple");
+	            primaryStage.show();
+	        } catch (Exception ex) {
+	            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
