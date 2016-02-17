7465bfddb3adb616d03b770d799251db2782ed4b
Novanet- <gamemaster8888@gmail.com>
2016-02-17 23:36:37 +0000

    Set min dimensions

--- RunwayRedeclaration/src/application/Main.java
+++ RunwayRedeclaration/src/application/Main.java
@@ -8,7 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+s
 public class Main extends Application
 {
 	@Override
