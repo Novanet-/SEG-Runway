c0dd61b774be892a90d624f3b8241ab36ce4520f
Will <Will@Will-PC>
2016-02-19 01:10:31 +0000

    Changed program title

--- RunwayRedeclaration/src/application/Main.java
+++ RunwayRedeclaration/src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application
 			AnchorPane page = (AnchorPane) FXMLLoader.load(getClass().getClassLoader().getResource("MainScreen.fxml"));
 			Scene scene = new Scene(page);
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("FXML is Simple");
+			primaryStage.setTitle("Runway Redeclaration");
 			primaryStage.setMinWidth(1000);
 			primaryStage.setMinHeight(600);
 			
