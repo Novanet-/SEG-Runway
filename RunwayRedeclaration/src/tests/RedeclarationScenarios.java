770aa11b931a357631d85c5909236279760d68b8
Daniel Oakey <dso1g13@soton.ac.uk>
2016-03-08 22:38:15 +0000

    Fixed redeclaration tests based on scenarios

--- RunwayRedeclaration/src/tests/RedeclarationScenarios.java
+++ RunwayRedeclaration/src/tests/RedeclarationScenarios.java
@@ -19,18 +19,18 @@ public class RedeclarationScenarios
 		double delta = 0.01;
 
 		double TORA = 3902;
-		double TODA = 3902;
-		double ASDA = 3902;
-		double LDA = 3595;
+		double TODA = TORA;
+		double ASDA = TORA;
+		double displacedThresh = 306;
+		double LDA = TORA - displacedThresh;
 		double blastProtect = 300.0;
 		double distFromThresh = -50; // Negative - before thresh
-		double displacedThresh = 306;
 		double obstHeight = 12.0;
 
 		double newTORA = 3346;
-		double newASDA = 3346;
 		double newTODA = 3346;
-		double newLDA = 2985;
+		double newASDA = 3346;
+		double newLDA = 2986; //
 
 		Runway r = new Runway(0, "09L", TORA, TODA, ASDA, LDA, displacedThresh);
 		Obstacle o = new Obstacle("tiny_plane", obstHeight, distFromThresh, blastProtect);
@@ -54,17 +54,17 @@ public class RedeclarationScenarios
 		double delta = 0.01;
 
 		double TORA = 3902;
-		double TODA = 3902;
-		double ASDA = 3902;
-		double LDA = 3595;
+		double TODA = TORA;
+		double ASDA = TORA;
+		double displacedThresh = 0;
+		double LDA = TORA - displacedThresh;
 		double blastProtect = 300.0;
 		double distFromThresh = 3646; // Positive - after thresh
-		double displacedThresh = 306;
 		double obstHeight = 12;
 		
 		double newTORA = 2986;
-		double newASDA = 2986;
 		double newTODA = 2986;
+		double newASDA = 2986;
 		double newLDA = 3346;
 		
 		Runway r = new Runway(0, "27R", TORA, TODA, ASDA, LDA, displacedThresh);
@@ -89,17 +89,17 @@ public class RedeclarationScenarios
 		double delta = 0.01;
 
 		double TORA = 3660;
-		double TODA = 3660;
-		double ASDA = 3660;
-		double LDA = 3660;
+		double TODA = TORA;
+		double ASDA = TORA;
+		double displacedThresh = 307;
+		double LDA = TORA - displacedThresh;
 		double blastProtect = 300.0;
 		double distFromThresh = 2853;
-		double displacedThresh = 307;
 		double obstHeight = 25.0;
 
 		double newTORA = 1850;
-		double newASDA = 1850;
 		double newTODA = 1850;
+		double newASDA = 1850;
 		double newLDA = 2553;
 
 		Runway r = new Runway(0, "09R", TORA, TODA, ASDA, LDA, displacedThresh);
@@ -119,22 +119,22 @@ public class RedeclarationScenarios
 	public void test2Scenario27L()
 	{
 		// Values from Scenario 2
-		// 27L Take off towards, land towards
+		// 27L Take off away, land over
 		
 		double delta = 0.01;
 
 		double TORA = 3660;
-		double TODA = 3660;
-		double ASDA = 3660;
-		double LDA = 3660;
+		double TODA = TORA;
+		double ASDA = TORA;
+		double displacedThresh = 0;
+		double LDA = TORA - displacedThresh;
 		double blastProtect = 300.0;
 		double distFromThresh = 500;
-		double displacedThresh = 307;
 		double obstHeight = 25;
 		
 		double newTORA = 2860;
-		double newASDA = 2860;
 		double newTODA = 2860;
+		double newASDA = 2860;
 		double newLDA = 1850;
 		
 		Runway r = new Runway(0, "27L", TORA, TODA, ASDA, LDA, displacedThresh);
@@ -159,17 +159,17 @@ public class RedeclarationScenarios
 		double delta = 0.01;
 
 		double TORA = 3660;
-		double TODA = 3660;
-		double ASDA = 3660;
-		double LDA = 3353;
+		double TODA = TORA;
+		double ASDA = TORA;
+		double displacedThresh = 307;
+		double LDA = TORA - displacedThresh;
 		double blastProtect = 300.0;
-		double distFromThresh = -50; // Negative - before thresh
-		double displacedThresh = 306;
+		double distFromThresh = 150;
 		double obstHeight = 15.0;
 
 		double newTORA = 2903;
-		double newASDA = 2903;
 		double newTODA = 2903;
+		double newASDA = 2903;
 		double newLDA = 2393;
 
 		Runway r = new Runway(0, "09L", TORA, TODA, ASDA, LDA, displacedThresh);
@@ -194,18 +194,18 @@ public class RedeclarationScenarios
 		double delta = 0.01;
 
 		double TORA = 3902;
-		double TODA = 3902;
+		double TODA = TORA;
 		double ASDA = 3902;
-		double LDA = 3595;
+		double displacedThresh = 0;
+		double LDA = TORA - displacedThresh;
 		double blastProtect = 300.0;
-		double distFromThresh = 3646; // Positive - after thresh
-		double displacedThresh = 306;
-		double obstHeight = 12;
+		double distFromThresh = 3203;
+		double obstHeight = 15;
 		
-		double newTORA = 2986;
-		double newASDA = 2986;
-		double newTODA = 2986;
-		double newLDA = 3346;
+		double newTORA = 2393;
+		double newTODA = 2393;
+		double newASDA = 2393;
+		double newLDA = 2903;
 		
 		Runway r = new Runway(0, "27R", TORA, TODA, ASDA, LDA, displacedThresh);
 		Obstacle o = new Obstacle("tiny_plane", obstHeight, distFromThresh, blastProtect);
@@ -223,24 +223,24 @@ public class RedeclarationScenarios
 	@Test
 	public void test4Scenario09L()
 	{
-		// Values from Scenario 1
-		// 09L Take off away, land over
+		// Values from Scenario 4
+		// 09L Take off towards, land towards
 
 		double delta = 0.01;
 
-		double TORA = 3902;
-		double TODA = 3902;
-		double ASDA = 3902;
-		double LDA = 3595;
-		double blastProtect = 300.0;
-		double distFromThresh = -50; // Negative - before thresh
+		double TORA = 3884;
+		double TODA = TORA;
+		double ASDA = TORA;
 		double displacedThresh = 306;
-		double obstHeight = 12.0;
+		double LDA = TORA - displacedThresh;
+		double blastProtect = 300.0;
+		double distFromThresh = 3546;
+		double obstHeight = 20.0;
 
-		double newTORA = 3346;
-		double newASDA = 3346;
-		double newTODA = 3346;
-		double newLDA = 2985;
+		double newTORA = 2792;
+		double newTODA = 2792;
+		double newASDA = 2792;
+		double newLDA = 3246;
 
 		Runway r = new Runway(0, "09L", TORA, TODA, ASDA, LDA, displacedThresh);
 		Obstacle o = new Obstacle("tiny_plane", obstHeight, distFromThresh, blastProtect);
@@ -258,24 +258,24 @@ public class RedeclarationScenarios
 	@Test
 	public void test4Scenario27R()
 	{
-		// Values from Scenario 1
-		// 27R Take off towards, land towards
+		// Values from Scenario 4
+		// 27R Take off away, land over
 		
 		double delta = 0.01;
 
-		double TORA = 3902;
-		double TODA = 3902;
-		double ASDA = 3902;
-		double LDA = 3595;
+		double TORA = 3884;
+		double TODA = TORA;
+		double ASDA = TORA;
+		double displacedThresh = 0;
+		double LDA = TORA - displacedThresh;
 		double blastProtect = 300.0;
-		double distFromThresh = 3646; // Positive - after thresh
-		double displacedThresh = 306;
-		double obstHeight = 12;
+		double distFromThresh = 50;
+		double obstHeight = 20;
 		
-		double newTORA = 2986;
-		double newASDA = 2986;
-		double newTODA = 2986;
-		double newLDA = 3346;
+		double newTORA = 3534;
+		double newTODA = 3534;
+		double newASDA = 3534;
+		double newLDA = 2774;
 		
 		Runway r = new Runway(0, "27R", TORA, TODA, ASDA, LDA, displacedThresh);
 		Obstacle o = new Obstacle("tiny_plane", obstHeight, distFromThresh, blastProtect);
