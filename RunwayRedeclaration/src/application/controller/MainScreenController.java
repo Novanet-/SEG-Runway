a2ce61ceb41c2101a83629ad716fb13e47731a8d
Jack Jack Clarke <jnc1g14@soton.ac.uk>
2016-03-27 21:15:38 +0100

    Fonts fixed to match design.

--- RunwayRedeclaration/src/application/controller/MainScreenController.java
+++ RunwayRedeclaration/src/application/controller/MainScreenController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import org.controlsfx.control.Notifications;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.Objects;
@@ -203,6 +204,10 @@ public class MainScreenController
 				}
 				updateObstacleList();
 				updateOriginalParameters();
+				Notifications.create()
+						.title("Obstacle removed")
+						.text(currentRunway.getObstacle().getName() + " obstacle removed.")
+						.showWarning();
 			}
 			else
 			{
